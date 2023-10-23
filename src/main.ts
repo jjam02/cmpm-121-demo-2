4854a8741de7eb554931f67eae98cc667d218253
Jonathan Alvarez <jalva100@ucsc.edu>
2023-10-22 19:22:03 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,21 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Jonathan Paint";
+const canWidth = 256;
+const canHeight = 256;
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = canWidth;
+canvas.height = canHeight;
+canvas.id = "draw";
+app.append(canvas);
+const context = canvas.getContext("2d")!;
+
+context.fillStyle = "white";
+context.fillRect(0, 0, canWidth, canHeight);
